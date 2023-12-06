3fb999251dc354484be37ba944278573fa814c61
sjmillius <sjmillius@gmail.com>
2023-12-06 09:57:49 +0100

    get_starting_sequence

--- p3bo.py
+++ p3bo.py
@@ -1,4 +1,6 @@
 """ This is the file that contains the main P3BO method that you are supposed to implement. """
+import math
+import random
 from typing import List
 
 import flexs.explorer
@@ -13,9 +15,27 @@ protein_alphabet = "ACDEFGHIKLMNPQRSTVWY"
 optimal_sequence = "MKYTKVMRYQIIKPLNAEWDELGMVLRDIQKETRAALNKTIQLCWEYQGFSADYKQIHGQYPKPKDVLGYTSMHGYAYDRLKNEFSKIASSNLSQTIKRAVDKWNSDLKEILRGDRSIPNFRKDCPIDIVKQSTKIQKCNDGYVLSLGLINREYKNELGRKNGVFDVLIKANDKTQQTILERIINGDYTYTASQIINHKNKWFINLTYQFETKETALDPNNVMGVDLGIVYPVYIAFNNSLHRYHIKGGEIERFRRQVEKRKRELLNQGKYCGDGRKGHGYATRTKSIESISDKIARFRDTCNHKYSRFIVDMALKHNCGIIQMEDLTGISKESTFLKNWTYYDLQQKIEYKAREAGIQVIKIEPQYTSQRCSKCGYIDKENRQEQATFKCIECGFKTNADYNAARNIAIPNIDKIIRKTLKMQ"
 
 
-def get_starting_sequence(base_sequence: str, identity_percent: float) -> str:
-    """This function returns a sequence that is identity_percent identical to the given base sequence"""
-    pass
+def mutate_with_alphabet(alphabet: str, original: str) -> str:
+    """Samples a new value from an alphabet."""
+    return random.choice([c for c in alphabet if c != original])
+
+
+def get_starting_sequence(
+    alphabet: str, base_sequence: str, identity_percent: float
+) -> str:
+    """This function returns a sequence that is identity_percent identical to the given base sequence.
+
+    Args:
+      alphabet: The alphabet to use.
+      base_sequence: The sequence to modify.
+      identity_percent: The fraction of positions to keep intact.
+    """
+    n = len(base_sequence)
+    num_mutations = math.ceil((1.0 - identity_percent / 100.0) * n)
+    mutant = list(base_sequence)
+    for pos in random.sample(population=range(n), k=num_mutations):
+        mutant[pos] = mutate_with_alphabet(alphabet=alphabet, original=mutant[pos])
+    return "".join(mutant)
 
 
 class P3bo:
